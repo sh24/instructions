3a58445220fe480ae53ce8aacbd3a84b2b5bf1b1
Tom Sabin <tom.sabin@unboxedconsulting.com>
2014-08-06 15:30:01 +0100

    Replace slides JS module with new one

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,4 @@
 (function ($) {
-  $("#slides").slidesjs({
-    width: 400,
-    height: 566,
-    pagination: {
-      active: false
-    },
-    navigation: {
-      active: false,
-      effect: 'slide'
-    }
-  });
-
-  $("#slides a").click(function (e) {
-    e.preventDefault();
-  });
+  var $slideshow = $('.slideshow').slides(),
+      api = $slideshow.data('slides');
 })(jQuery);
